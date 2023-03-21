b08c85db30938e185ea86d026390b9fc7e4a42f1
Bryan-Codes <bry5884@gmail.com>
2023-03-20 23:53:12 -0700

    Troubleshooting pages deployment

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
-app.use(express.static('public'));
+app.use(express.static('assets'));
 
 // Routes
 app.use('/api', api);
